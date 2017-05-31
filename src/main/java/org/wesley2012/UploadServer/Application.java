406310d76cd6ef566315065dac9cbcaa34aae61c
wesley2012 <wesley.mail@qq.com>
2017-05-31 12:00:23 +0800

    ok

--- src/main/java/org/wesley2012/UploadServer/Application.java
+++ src/main/java/org/wesley2012/UploadServer/Application.java
@@ -1,10 +1,20 @@
+package org.wesley2012.UploadServer;
 
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.MultipartFilter;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class Application {
     
@@ -18,6 +28,11 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
 
 }
